8ee4843de22ab060d9dbc9f43c93a1aa70753e18
ediv4ldoantonio <ediv4ldoantonio@gmail.com>
2024-05-11 15:28:34 +0100

    Add read logic

---
+++ Models/ResponseData.cs
@@ -0,0 +1,10 @@
+﻿namespace CANSAT_UI.Models;
+
+public class ResponseData
+{
+    public required string location { get; set; }
+    public required string date { get; set; }
+    public required string time { get; set; }
+    public required double temperature { get; set; }
+    public required double humidity { get; set; }
+}
--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -1,6 +1,73 @@
-﻿namespace CANSAT_UI.ViewModels;
+﻿using CANSAT_UI.Models;
+using CANSAT_UI.Services.Contracts;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace CANSAT_UI.ViewModels;
+
 
 public partial class DashboardViewModel : ViewModelBase
 {
+    #region ObservableProperties
+    [ObservableProperty]
+    private string location = string.Empty;
 
+    [ObservableProperty]
+    private string date = string.Empty;
+
+    [ObservableProperty]
+    private string time = string.Empty;
+
+    [ObservableProperty]
+    private string temperature = string.Empty;
+
+    [ObservableProperty]
+    private string humidity = string.Empty;
+    #endregion
+
+    HttpClient client = new HttpClient();
+
+    // O token do Blynk
+    const string token = "VdUhb7q81RGZ3vhGCoy-porxIZCxs-mj";
+
+    private readonly ISerialCommunicationService serialCommunicationService;
+
+    public DashboardViewModel(ISerialCommunicationService serialCommunicationService)
+    {
+        this.serialCommunicationService = serialCommunicationService;
+        InitializeDataReceived();
+    }
+
+    private void InitializeDataReceived()
+    {
+        this.serialCommunicationService.DataReceived += OnDataReceived;
+    }
+
+    private async void OnDataReceived(object? sender, string data)
+    {
+        try
+        {
+            ResponseData? responseData = JsonSerializer.Deserialize<ResponseData>(data);
+
+            if (responseData != null)
+            {
+                Location = responseData.location;
+                Temperature = $"{responseData.temperature} ºC";
+                Date = responseData.date;
+                Time = responseData.time;
+                Humidity = $"{responseData.humidity}%";
+            }
+            string pinoVirtual1 = "v0";
+            string pinoVirtual2 = "v1";
+            string pinoVirtual3 = "v2";
+            await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual1}={responseData.humidity}");
+            await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual2}={responseData.temperature}");
+            await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual3}={responseData.location}");
+        }
+        catch 
+        {
+            
+        }
+    }
 }
