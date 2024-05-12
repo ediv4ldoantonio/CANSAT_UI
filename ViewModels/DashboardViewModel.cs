f58a83bd78358528a1b9d8b9eaf3b39c21a33f64
ediv4ldoantonio <ediv4ldoantonio@gmail.com>
2024-05-12 19:38:30 +0100

    Add chart functionality

--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -2,6 +2,7 @@
 using CANSAT_UI.Repositories;
 using CANSAT_UI.Services.Contracts;
 using CommunityToolkit.Mvvm.ComponentModel;
+using LiveCharts;
 using System.Net.Http;
 using System.Text.Json;
 
@@ -29,6 +30,9 @@ public partial class DashboardViewModel : ViewModelBase
 
     readonly HttpClient client = new HttpClient();
     readonly IDataRepository dataRepository;
+
+    [ObservableProperty]
+    private ChartValues<double> temperatureValues;
 
     // O token do Blynk
     const string token = "6ju-aH1fAZD97gvIwbui6qceTEWIF0Fm";
@@ -61,18 +65,18 @@ public partial class DashboardViewModel : ViewModelBase
                 Time = responseData.time;
                 Humidity = $"{responseData.humidity}%";
 
+                TemperatureValues.Add(responseData.humidity);
+                OnPropertyChanged(nameof(TemperatureValues));
+
                 string pinoVirtual1 = "v0";
                 string pinoVirtual2 = "v1";
                 string pinoVirtual3 = "v2";
-                string pinoVirtual4 = "v3";
-                string pinoVirtual5 = "v4";
 
                 await dataRepository.Create(new Data { Date = Date, Humidity = responseData.humidity, Location = Location, Temperature = responseData.temperature, Time = Time, Timestamp = DateTime.Now });
+
                 await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual1}={responseData.humidity}");
                 await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual2}={responseData.temperature}");
                 await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual3}={responseData.location}");
-                await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual4}={responseData.date}");
-                await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual5}={responseData.time}");
             }
         }
         catch
