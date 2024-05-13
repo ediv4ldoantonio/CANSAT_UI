bc572f32e28805eac33f2974406d7ea57a20b248
ediv4ldoantonio <ediv4ldoantonio@gmail.com>
2024-05-13 15:30:02 +0100

    Add the 30 sec timer

--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -5,6 +5,11 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using LiveCharts;
 using System.Net.Http;
 using System.Text.Json;
+using System;
+using System.Timers;
+using Timer = System.Timers.Timer;
+using System.Windows;
+using Org.BouncyCastle.Asn1.Ocsp;
 
 namespace CANSAT_UI.ViewModels;
 
@@ -38,14 +43,30 @@ public partial class DashboardViewModel : ViewModelBase
     const string token = "6ju-aH1fAZD97gvIwbui6qceTEWIF0Fm";
 
     private readonly ISerialCommunicationService serialCommunicationService;
-
+    private readonly Timer timer;
+    private double _humidity;
+    private double _temperature;
     public DashboardViewModel(ISerialCommunicationService serialCommunicationService, IDataRepository dataRepository)
     {
         this.dataRepository = dataRepository;
+        this.temperatureValues = [];
+
+        timer = new Timer
+        {
+            Interval = 30000
+        };
+
+        timer.Elapsed += Timer_Elapsed;
+        timer.Start();
+
         this.serialCommunicationService = serialCommunicationService;
         InitializeDataReceived();
     }
 
+    private async void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+    {
+        await dataRepository.Create(new Data { Date = Date, Humidity = _humidity, Location = Location, Temperature = _temperature, Time = Time, Timestamp = DateTime.Now });
+    }
     private void InitializeDataReceived()
     {
         this.serialCommunicationService.DataReceived += OnDataReceived;
@@ -55,7 +76,7 @@ public partial class DashboardViewModel : ViewModelBase
     {
         try
         {
-            ResponseData? responseData = JsonSerializer.Deserialize<ResponseData>(data);
+            Models.ResponseData? responseData = JsonSerializer.Deserialize<Models.ResponseData>(data);
 
             if (responseData != null)
             {
@@ -64,6 +85,8 @@ public partial class DashboardViewModel : ViewModelBase
                 Date = responseData.date;
                 Time = responseData.time;
                 Humidity = $"{responseData.humidity}%";
+                _humidity = responseData.humidity;
+                _temperature = responseData.temperature;
 
                 TemperatureValues.Add(responseData.humidity);
                 OnPropertyChanged(nameof(TemperatureValues));
@@ -72,7 +95,6 @@ public partial class DashboardViewModel : ViewModelBase
                 string pinoVirtual2 = "v1";
                 string pinoVirtual3 = "v2";
 
-                await dataRepository.Create(new Data { Date = Date, Humidity = responseData.humidity, Location = Location, Temperature = responseData.temperature, Time = Time, Timestamp = DateTime.Now });
 
                 await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual1}={responseData.humidity}");
                 await client.GetAsync($"https://blynk.cloud/external/api/update?token={token}&{pinoVirtual2}={responseData.temperature}");
